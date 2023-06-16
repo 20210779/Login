f28c19c540f1a6bfb15135d4e3b18d213bd7546f
ESTUDIANTE <ESTUDIANTE@DESKTOP-86K5154>
2023-06-16 11:53:14 -0600

    Base de datos creada

---
+++ src/archivos/Base_de_datos.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package archivos;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class Base_de_datos {
+    
+    /*CREATE DATABASE LoginRemedial;
+      GO
+
+	USE LoginRemedial
+	GO
+
+	CREATE TABLE tbDatos
+	(
+	 IDUser int identity(1,1) primary key,
+	 UserName varchar(20),
+	 Contraseña varchar(30)
+	);
+	GO
+
+	INSERT INTO tbDatos (UserName,Contraseña)VALUES('Star','Mark96');
+	SELECT *FROM tbSesion;
+	SELECT * from tbSesion  WHERE UserName='Star' AND Contraseña='Mark96';*/
+    
+}
